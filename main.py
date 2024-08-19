5ff551c70014d8efd7805c73cb310720f1cf08d6
Skanda Kaashyap <skandakk@gmail.com>
2024-08-19 14:36:15 -0500

    remove main function

--- main.py
+++ main.py
@@ -6,25 +6,20 @@ import sys
 from bot_def import new_bot
 
 
-def main():
-    api_server.keep_alive()
+api_server.keep_alive()
 
-    # just doing it this way because we're defining this class by overriding the actual methods and also
-    # using decorators. just want to do it this way so that we can isolate all the bot configuration code
-    # to a single file
-    bot = new_bot()
+# just doing it this way because we're defining this class by overriding the actual methods and also
+# using decorators. just want to do it this way so that we can isolate all the bot configuration code
+# to a single file
+bot = new_bot()
 
-    # load secrets
-    DISCORD_BOT_TOKEN_ENV_KEY = "DISCORD_BOT_TOKEN"
-    load_dotenv()
-    discord_bot_token_value = os.getenv(DISCORD_BOT_TOKEN_ENV_KEY)
-    if discord_bot_token_value is None:
-        logging.error(
-            f"missing .env file or missing variable: {DISCORD_BOT_TOKEN_ENV_KEY}")
-        sys.exit(1)
+# load secrets
+DISCORD_BOT_TOKEN_ENV_KEY = "DISCORD_BOT_TOKEN"
+load_dotenv()
+discord_bot_token_value = os.getenv(DISCORD_BOT_TOKEN_ENV_KEY)
+if discord_bot_token_value is None:
+    logging.error(
+        f"missing .env file or missing variable: {DISCORD_BOT_TOKEN_ENV_KEY}")
+    sys.exit(1)
 
-    bot.run(discord_bot_token_value)
-
-
-if __name__ == "__main__":
-    main()
+bot.run(discord_bot_token_value)
